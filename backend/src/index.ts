56ac1a1070fe047ac44e0f554356add3f6575a74
Pravin Choudhary <pravinchoudhary.aesxa29@gmail.com>
2025-03-02 12:42:50 +0530

    sending forecast Dates And Time

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,8 @@ import express from "express";
 import cors from "cors";
 import AiRouter from "./routes/ai";
 import CropRouter from "./routes/crop";
+import WeatherRouter from "./routes/weather";
+
 
 const app = express();
 
@@ -10,6 +12,11 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/v1/ai", AiRouter);
 app.use("/api/v1/crop", CropRouter);
+app.use("/api/v1/weather", WeatherRouter);
+
+app.get('/' , (req , res) => {
+    res.send("Root working ");
+})
 
 app.listen(3000 , () => {
     console.log("Server started on Port : 3000");
--- backend/src/routes/weather.ts
+++ backend/src/routes/weather.ts
@@ -1,52 +1,52 @@
-import { Router } from "express";
+import { Router  } from "express";
 import axios from "axios";
 
 const WeatherRouter = Router();
 
 
-WeatherRouter.get("/forecast/:city", async (req, res) => {
+
+WeatherRouter.get("/forecast/:city", async (req , res) => {
+ 
+  const city =  req.params.city;
   
-    console.log(req.params);
-    
-    const city = req.params.city;
-  
-  
-    try {
-      const response  = await axios.get(`${process.env.BASE_API_URL}q=${city}&appid=${process.env.API_KEY}&units=metric`);
-  
-    const condition = response.data.weather[0].description;
-    const  temperature = response.data.main.temp;
-    const humidity = response.data.main.humidity;
-    const windSpeed = response.data.wind.speed;
-    const windDegree = response.data.wind.deg;
-    const pressure = response.data.main.pressure;
-  
-    console.log("weather Condition :" , condition);
-    console.log("weather Temp :" , temperature);
-    console.log("weather windSpeed :" , windSpeed);
-  
-    
-    const weatherData = {
-      city ,
-      temperature, 
-      condition,
-      humidity,
-      windSpeed,
-      windDegree,
-      pressure
+  try {
+     const response = await axios.get(`
+      https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=4bab098e1ac811aed6c44bb3c7aeb953&units=metric`
+      );
+
+      const forecasts = response.data.list;
+      if (forecasts.length === 0) {
+        return res.status(404).json({ error: "No forecast data available" });
     }
-  
+
+    const selectedForecasts = [forecasts[0]];
+
+    const uniqueDates: string[] = [];  
+    for (let i = 0; i < forecasts.length; i++) {
+        const entry = forecasts[i];
+        const [date, time] = entry.dt_txt.split(" "); 
+
+        if (time === "12:00:00" && !uniqueDates.includes(date)) {
+            uniqueDates.push(date);
+            selectedForecasts.push(entry);
+        }
+
+        if (selectedForecasts.length === 5) break; 
+    }
     res.json({
-      weatherData
-    });
-  
-    } catch (error) {
-      console.log('Error in fetching Data :' , error);
-      
-      res.status(500).json({error : 'Error in fetching Data :'});
-    }
+      city,
+      forecast: selectedForecasts,
+  });
+
+  } catch (error) {
+        console.log('Error in fetching Data :' , error);
+        res.status(500).json({error : 'Error in fetching Data :'});
+  }
     
   });
 
 
+
+
 export default WeatherRouter;
+
